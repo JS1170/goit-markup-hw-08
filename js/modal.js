99bfbec4fc0758871ef375ae70da4e88cedbd70c
Yevgen Savchuk <107425775+JS1170@users.noreply.github.com>
2022-07-19 01:12:25 +0300

    pages

--- js/modal.js
+++ js/modal.js
@@ -9,27 +9,7 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle("modal-open");
     refs.modal.classList.toggle("is-hidden");
   }
-})();
-
-(() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
-
-  menuBtnRef.addEventListener("click", (e) => {
-    // const expanded =
-    //   menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-    //   menuBtnRef.setAttribute("aria-expanded", !expanded);
-    if (!e.target.closest(".menu-button").classList.contains("is-open")) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "auto";
-    }
-
-    menuBtnRef.classList.toggle("is-open");
-
-    mobileMenuRef.classList.toggle("is-open");
-  });
-
 })();
